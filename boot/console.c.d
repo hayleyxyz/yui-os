e935764873fe3df753bf08b62c815ed261104bd6
yui <i@yuikon.nu>
2018-03-03 23:41:20 +0000

    remove .d files

--- boot/console.c.d
+++
@@ -1,3 +0,0 @@
-boot/console.c.o: boot/console.c include/types.h
-
-include/types.h:
--- kernel/console.c.d
+++
@@ -1,6 +0,0 @@
-kernel/console.c.o: kernel/console.c include/types.h \
- include/libc/printf.h
-
-include/types.h:
-
-include/libc/printf.h:
